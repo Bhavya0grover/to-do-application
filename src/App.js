6f742aea960546a23b54cb3f268085a5d8a29a5b
Bhavya Grover <m8003232020@gmail.com>
2024-05-29 00:11:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        <h1>To-Do List</h1>
+        <TodoForm addTodo={addTodo} />
+        <TodoList todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
